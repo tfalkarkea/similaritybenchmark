d7c19f1886ad600aadaa6470b0e4930f5ab658b7
Taylor Falk <tfalk@arkeabio.com>
2023-06-01 09:09:14 -0400

    Add multithreading to correlations py script

--- 2-Correlations.py
+++ 2-Correlations.py
@@ -2,6 +2,9 @@ import os
 import sys
 import glob
 import scipy.stats
+import multiprocessing
+from itertools import repeat
+from multiprocessing import Pool
 
 SIZE = 4
 
@@ -16,19 +19,25 @@ def getCorrelations(fname, outfile):
                 corr = scipy.stats.spearmanr(ref_corr, similarities)
             f.write("%f\n" % corr[0])
 
+def run_iteration(iternum):
+    print iternum,
+    if not os.path.isdir(os.path.join(benchmark, "correlations")):
+        os.mkdir(os.path.join(benchmark, "correlations"))
+    inputdir = os.path.join(benchmark, "similarities", "%d" % iternum)
+    outputdir = os.path.join(benchmark, "correlations", "%d" % iternum)
+    if not os.path.isdir(outputdir):
+        os.mkdir(outputdir)
+    for fname in glob.glob(os.path.join(inputdir, "*.txt")):
+        name = os.path.basename(fname)
+        getCorrelations(fname, os.path.join(outputdir, name))
+
+
 if __name__ == "__main__":
     for benchmark in ["SingleAssay", "MultiAssay"]:
         # Note that the following loop is completely parallelisable
         # e.g. you could run from 0->500 on one CPU and from 500->1000 on
         #      another to finish in half the time
-        for i in range(1000):
-            print i,
-            if not os.path.isdir(os.path.join(benchmark, "correlations")):
-                os.mkdir(os.path.join(benchmark, "correlations"))
-            inputdir = os.path.join(benchmark, "similarities", "%d" % i)
-            outputdir = os.path.join(benchmark, "correlations", "%d" % i)
-            if not os.path.isdir(outputdir):
-                os.mkdir(outputdir)
-            for fname in glob.glob(os.path.join(inputdir, "*.txt")):
-                name = os.path.basename(fname)
-                getCorrelations(fname, os.path.join(outputdir, name))
+        pool = Pool(multiprocessing.cpu_count())
+        inputs = zip(range(15), repeat(benchmark))
+        print(inputs)
+        pool.map(run_iteration, inputs)
